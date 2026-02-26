ba4a04c23f50c1e70f0f7ffab3e0c154280165ea
PeeterLond <Peeter.Lond@outlook.com>
2026-02-26 18:00:21 +0200

    ci/cd- changes and test

--- frontend/app/(public)/login/page.tsx
+++ frontend/app/(public)/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
     process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center content-center gap-4">
+    <div className="flex flex-col min-h-screen items-center justify-center content-center gap-4 bg-red-700">
       <h1 className="text-2xl font-bold text-center">Login</h1>
       <a href={`${publicBackendUrl}/oauth2/authorization/google`}>
         <button
